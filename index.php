c4532bdd377d4280910ffae32b56c02c0c07006b
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-09 18:09:35 +0200

    page d'aceuil

--- index.php
+++ index.php
@@ -4,26 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Sommaire</title>
 </head>
 <body>
-    <h1>Arnaud-Bourique Sylvain</h1>
-
-    <section id="select">
-
-        <?php 
-           //enumere le dossier test
-           $cont = scandir('projet');
-
-           foreach($cont as $value){
-               if($value[0] != '.' && is_dir($value)){
-                   $files = scandir($value);
-                   for($i = 0; $i < count($files); $i++){
-                      echo '<a href="projet/'.$value.'/'.$files[$i].'">'.$files[$i].'</a></br>';
-               }}
-           }
-        ?>
-
+    <section id="perso">
+        <p>Arnaud-Bourique</p>
+        <p>Sylvain</p>
+        <p>24 ans</p>
+        <p>Rhone - St Bonnet de mure</p>
+        <p>9 Impasse du Matinau</p>
+        <p>arnaudbouriquesylvain@gmail.com</p>
+        <ol>
+            <h6>Restauration</h6>
+            <li>- Equipier</li>
+            <li>- Chef d'équipe</li>
+            <li>- Manager de zone</li>
+            <li>- Manager</li>
+            <h6>Hors McDo</h6>
+            <li>- Barman/Serveur</li>
+            <li>- Manutention</li>
+        </ol>
+        <ol>
+            <h6>Experience Developpeur</h6>
+            <li>Simplon</li>
+            <li>HackaPress 2017</li>
+        </ol>
+        <p>GitHub : ABSylvain</p>
     </section>
+    <section class="fenetre"></section>
+    <section class="fenetre"></section>
+    <section class="fenetre"></section>
 </body>
 </html>
--- test/php.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    foreach($_POST['prenom'] as $valeur)
-{
-   echo "La checkbox $valeur a été cochée<br>";
-}
-?>
