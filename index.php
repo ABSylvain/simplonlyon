a28dd10ab90502c5da08d0d486b6ccc140de0d93
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-10 23:54:16 +0200

    manque le php mais stucture faite

--- index.php
+++ index.php
@@ -9,31 +9,33 @@
 </head>
 <body>
     <section id="perso">
-        <p>Arnaud-Bourique</p>
-        <p>Sylvain</p>
-        <p>24 ans</p>
-        <p>Rhone - St Bonnet de mure</p>
-        <p>9 Impasse du Matinau</p>
-        <p>arnaudbouriquesylvain@gmail.com</p>
-        <ol>
-            <h6>Restauration</h6>
-            <li>- Equipier</li>
-            <li>- Chef d'équipe</li>
-            <li>- Manager de zone</li>
-            <li>- Manager</li>
-            <h6>Hors McDo</h6>
-            <li>- Barman/Serveur</li>
-            <li>- Manutention</li>
-        </ol>
-        <ol>
-            <h6>Experience Developpeur</h6>
-            <li>Simplon</li>
-            <li>HackaPress 2017</li>
-        </ol>
-        <p>GitHub : ABSylvain</p>
+        <div>
+            <p>Arnaud-Bourique</p>
+            <p>Sylvain</p>
+            <p>24 ans</p>
+            <p>Rhone - St Bonnet de mure</p>
+            <p>9 Impasse du Matinau</p>
+            <p>arnaudbouriquesylvain@gmail.com</p>
+            <ol>
+                <h6>Restauration</h6>
+                <li>- Equipier</li>
+                <li>- Chef d'équipe</li>
+                <li>- Manager de zone</li>
+                <li>- Manager</li>
+                <h6>Hors McDo</h6>
+                <li>- Barman/Serveur</li>
+                <li>- Manutention</li>
+            </ol>
+            <ol>
+                <h6>Experience Developpeur</h6>
+                <li>Simplon</li>
+                <li>HackaPress 2017</li>
+            </ol>
+            <p>GitHub : ABSylvain</p>
+        <div>
     </section>
-    <section class="fenetre"></section>
-    <section class="fenetre"></section>
-    <section class="fenetre"></section>
+    <section class="fenetre1"></section>
+    <section class="fenetre2"></section>
+    <section class="fenetre3"></section>
 </body>
 </html>
